b468fc90cfc2af974d186d4dcc8d6e09a01dc15d
Laptop <augustin.detoffoli@outlook.be>
2025-11-03 10:19:43 +0100

    ajout des limites sur le graphe ajout de fichier au gitignore

--- lecture_fichier.py
+++ lecture_fichier.py
@@ -6,18 +6,39 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolb
 from matplotlib.figure import Figure
 
 fenetre = ctk.CTk()
+zone_limite_1 = dict()
+zone_limite_2 = dict()
+zone_limite_3 = dict()
+
+def hune():
+    global zone_limite_1, zone_limite_2, zone_limite_3
+
+    zone_limite_1 = {
+        "x": [-132.5, -132.5, -117.5, -117.5, -132.5],
+        "y": [6.5, 118, 118, 6.5, 6.5]
+    }
+
+    zone_limite_2 = {
+        "x": [-112.5, -112.5, -107.5, -107.5, 107.5, 107.5, 112.5, 112.5, -112.5],
+        "y": [0, 59, 59, 118, 118, 59, 59, 0, 0]
+    }
+
+    zone_limite_3 = {
+        "x": [132.5, 132.5, 117.5, 117.5, 132.5],
+        "y": [118, 6.5, 6.5, 118, 118]
+    }
 
 
 def read_file():
     lignes_a_sauter = set()
-    with open("+25.txt", "r") as f:
+    with open("0.txt", "r") as f:
         lignes = f.readlines()
         for i, ligne in enumerate(lignes):
             if "Angle" in ligne:
                 lignes_a_sauter.update(range(0, i))
                 break
 
-    data_file = pandas.read_csv("+25.txt", sep= ";", 
+    data_file = pandas.read_csv("0.txt", sep= ";", 
                 skiprows = lambda x: x in lignes_a_sauter, skipfooter = 2,
                 engine="python", usecols=["Angle °", "cd", "X", "Y", "Tension V", "Courant A", "temperature ambiante °C"])
     return data_file
@@ -25,20 +46,32 @@ def read_file():
 
 def trace_graph():
     data = read_file()
-    fig = Figure(figsize=(5, 4), dpi=100)
+    data['Angle °'] = data['Angle °'].apply(lambda x: x-90)
+    hune()
+    fig = Figure(figsize=(6, 4), dpi=100)
     ax = fig.add_subplot(111)
+    
+    # plot des données 
     ax.plot(data["Angle °"], data["cd"], color="steelblue")
+    #plot des limite remplie (pour plus de classe)
+    ax.plot(zone_limite_1["x"], zone_limite_1["y"], color="red", linestyle="--", alpha = 0.5)
+    ax.fill(zone_limite_1["x"], zone_limite_1["y"], color="red", alpha=0.2, label="Zone limite 1")
+    ax.plot(zone_limite_2["x"], zone_limite_2["y"], color="red", linestyle="--", alpha = 0.5)
+    ax.fill(zone_limite_2["x"], zone_limite_2["y"], color="red", alpha=0.2, label="Zone limite 2")
+    ax.plot(zone_limite_3["x"], zone_limite_3["y"], color="red", linestyle="--", alpha = 0.5)
+    ax.fill(zone_limite_3["x"], zone_limite_3["y"], color="red", alpha=0.2, label="Zone limite 3")
+
     ax.set_title("Intensité lumineuse en fonction de l'angle")
     ax.set_xlabel("Angle (°)")
     ax.set_ylabel("Intensité lumineuse (cd)")
+    ax.grid()
     
-    data['Angle °'] = data['Angle °'].apply(lambda x: x-90)
+
 
     # Intégration dans Tkinter
     canvas = FigureCanvasTkAgg(fig, master=fenetre)
     canvas.draw()
     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-
     # Barre d'outils
     toolbar = NavigationToolbar2Tk(canvas, fenetre)
     toolbar.update()
@@ -59,9 +92,4 @@ def window():
 
 window()
 
-
-def hune():
-    zone_limite_1 = {-132.5:6.5, -132.5:118,-117.5:118,-117.5:6.5,-132.5:6.5}
-    zone_limite_2 = {-112.5:0, -112.5:59, -107.5:59, -107.5:118, 107.5:118, 107.5:59, 112.5:59, 112.5:0, -112.5:0}
-    zone_limite_3 = {132.5:118, 132.5:6.5, 117.5:6.5, 117.5:118, 132.5:118}
 
