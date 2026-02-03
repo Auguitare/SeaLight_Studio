f5021c297a1a023edd0823614a7225925646f99a
Auguitare_Desktop <augustin.detoffoli@outlook.be>
2026-02-04 00:57:33 +0100

    replace plot by a scatter

--- tab_colo.py
+++ tab_colo.py
@@ -5,11 +5,13 @@ def trace_graph(data, ax):
     ax.clear()
     
     # Tracer les données
-    ax.plot(data["X"], data["Y"], color="steelblue")
+    ax.scatter(data["X"], data["Y"], c='black', s=50, alpha=0.6)
     
     ax.set_title("Diagramme chromatique")
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
+    ax.set_xlim([0, 1])
+    ax.set_ylim([0, 1])
     ax.minorticks_on()
     ax.grid(which="major", alpha=0.7)
     ax.grid(which="minor", linestyle="--", linewidth=0.5, alpha=0.4)
