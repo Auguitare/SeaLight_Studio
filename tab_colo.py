338ca7f3bc2fabc40a87a7fc5ec02211056d1b19
Auguitare_Desktop <augustin.detoffoli@outlook.be>
2026-02-04 02:29:37 +0100

    first test of limits in colorimetry

--- tab_colo.py
+++ tab_colo.py
@@ -15,3 +15,18 @@ def trace_graph(data, ax):
     ax.minorticks_on()
     ax.grid(which="major", alpha=0.7)
     ax.grid(which="minor", linestyle="--", linewidth=0.5, alpha=0.4)
+
+
+def trace_limit(ax):
+    """Trace les zones limites"""
+    zone_white = {"X":[0.525, 0.525, 0.452, 0.310, 0.31, 0.443, 0.525], "Y":[0.382, 0.44, 0.44, 0.348, 0.283, 0.382, 0.382]  }
+    ax.plot(zone_white["X"], zone_white["Y"], color='grey',linestyle = "--", alpha=0.5)
+
+    ax.set_title("Diagramme chromatique")
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_xlim([0, 1])
+    ax.set_ylim([0, 1])
+    ax.minorticks_on()
+    ax.grid(which="major", alpha=0.7)
+    ax.grid(which="minor", linestyle="--", linewidth=0.5, alpha=0.4)
