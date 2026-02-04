d80001d908800a6cb12d4ef20783358ec8a03f36
Auguitare_Desktop <augustin.detoffoli@outlook.be>
2026-02-04 01:57:58 +0100

    suppression des variable globale pour un return plus propre

--- zone.py
+++ zone.py
@@ -1,11 +1,6 @@
 import tkinter as tk
 
-zone_limite_1 = dict()
-zone_limite_2 = dict()
-zone_limite_3 = dict()
-
 def intensity_calc(range, inclinaison):
-    global zone_limite_1, zone_limite_2, zone_limite_3
     match range:
         case 1:
             max_power = 1.1
@@ -25,64 +20,62 @@ def intensity_calc(range, inclinaison):
     if(inclinaison != 0):
         max_power *= 0.5
 
-    zone_limite_1 ["y"]= [0.1*max_power, max_power, max_power, 0.1*max_power, 0.1*max_power]
-    zone_limite_2 ["y"]= [0, 0.5*max_power, 0.5*max_power, max_power, max_power, 0.5*max_power, 0.5*max_power, 0, 0]
-    zone_limite_3 ["y"]= [max_power, 0.1*max_power, 0.1*max_power, max_power, max_power]
+    return {
+        1: {"Y": [0.1*max_power, max_power, max_power, 0.1*max_power, 0.1*max_power]},
+        2: {"Y": [0, 0.5*max_power, 0.5*max_power, max_power, max_power, 0.5*max_power, 0.5*max_power, 0, 0]},
+        3: {"Y": [max_power, 0.1*max_power, 0.1*max_power, max_power, max_power]}
+    }
+
 
 def hune(range, inclinaison):
-    global zone_limite_1, zone_limite_2, zone_limite_3
-
-    intensity_calc(range, inclinaison)
+    zone = intensity_calc(range, inclinaison)
     
-    zone_limite_1 ["x"]= [-132.5, -132.5, -117.5, -117.5, -132.5]
-    zone_limite_2 ["x"]= [-112.5, -112.5, -107.5, -107.5, 107.5, 107.5, 112.5, 112.5, -112.5]
-    zone_limite_3 ["x"]= [132.5, 132.5, 117.5, 117.5, 132.5]
+    zone[1]["X"]= [-132.5, -132.5, -117.5, -117.5, -132.5]
+    zone[2]["X"]= [-112.5, -112.5, -107.5, -107.5, 107.5, 107.5, 112.5, 112.5, -112.5]
+    zone[3]["X"]= [132.5, 132.5, 117.5, 117.5, 132.5]
+
+    return zone
 
 
 def poupe(range, inclinaison):
-    global zone_limite_1, zone_limite_2, zone_limite_3
-
-    intensity_calc(range, inclinaison)
+    zone = intensity_calc(range, inclinaison)
     
-    zone_limite_1 ["x"]= [85, 85, 107.5, 107.5, 85]
-    zone_limite_2 ["x"]= [112.5, 112.5, 117.5, 117.5, 242.5, 242.5, 247.5, 247.5, 112.5]
-    zone_limite_3 ["x"]= [267.5, 267.5, 252.5, 252.5, 267.5]
+    zone[1]["X"]= [85, 85, 107.5, 107.5, 85]
+    zone[2]["X"]= [112.5, 112.5, 117.5, 117.5, 242.5, 242.5, 247.5, 247.5, 112.5]
+    zone[3]["X"]= [267.5, 267.5, 252.5, 252.5, 267.5]
+
+    return zone
 
 
 def babord(range, inclinaison):
-    global zone_limite_1, zone_limite_2, zone_limite_3
 
-    intensity_calc(range, inclinaison)
+    zone = intensity_calc(range, inclinaison)
     
-    zone_limite_1 ["x"]= [-30, -30, -3, -3, -30]
-    zone_limite_2 ["x"]= [0, 0, 0, 0, 107.5, 107.5, 112.5, 112.5, 0]
-    zone_limite_3 ["x"]= [142.5, 142.5, 117.5, 117.5, 142.5]
+    zone[1]["X"]= [-30, -30, -3, -3, -30]
+    zone[2]["X"]= [0, 0, 0, 0, 107.5, 107.5, 112.5, 112.5, 0]
+    zone[3]["X"]= [142.5, 142.5, 117.5, 117.5, 142.5]
+
+    return zone
 
 
 def tribord(range, inclinaison):
-    global zone_limite_1, zone_limite_2, zone_limite_3
 
-    intensity_calc(range, inclinaison)
+    zone = intensity_calc(range, inclinaison)
     
-    zone_limite_1 ["x"]= [-142.5, -142.5, -117.5, -117.5, -142.5]
-    zone_limite_2 ["x"]= [-112.5, -112.5, -107.5, -107.5, 0, 0, 0, 0, -112.5]
-    zone_limite_3 ["x"]= [30, 30, 3, 3, 30]
+    zone[1]["X"]= [-142.5, -142.5, -117.5, -117.5, -142.5]
+    zone[2]["X"]= [-112.5, -112.5, -107.5, -107.5, 0, 0, 0, 0, -112.5]
+    zone[3]["X"]= [30, 30, 3, 3, 30]
+
+    return zone
 
 
 def only_value():
-    global zone_limite_1, zone_limite_2, zone_limite_3
+    zone = intensity_calc(1, 0)
+    zone[1]["X"] = [0]
+    zone[1]["Y"] = [0]
+    zone[2]["X"] = [0]
+    zone[2]["Y"] = [0]
+    zone[3]["X"] = [0]
+    zone[3]["Y"] = [0]
 
-    zone_limite_1 = {
-        "x": [0],
-        "y": [0],
-    }
-
-    zone_limite_2 = {
-        "x": [0],
-        "y": [0],
-    }
-
-    zone_limite_3 = {
-        "x": [0],
-        "y": [0],
-    }
+    return zone
